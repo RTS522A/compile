47119920ac84ccda776d3ea8aea304049087d795
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-13 23:12:11 +0300

    Update testgen.cmd

--- lookfs/testgen.cmd
+++ lookfs/testgen.cmd
@@ -8,5 +8,5 @@ md "%D%"
 md "%D2%"
 echo. > "%F%"
 for %%i in (foo bar baz) do @( echo %%i > "%D%:%%i" && echo %%i > "%D%\%%i" && echo %%i > "%D2%\%%i" && echo %%i > "%F%:%%i" )
-junction -accepteula -nobanner "%D%\hello.junction" "%D2%"
+mklink k /D "%D%\hello.junction" "%D2%"
 popd & endlocal & goto :EOF
