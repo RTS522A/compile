47f124730b693a8338dcc5c826cc2cb6b132c3eb
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-13 23:29:38 +0300

    Update testgen.cmd

--- lookfs/testgen.cmd
+++ lookfs/testgen.cmd
@@ -1,6 +1,7 @@
 @echo off
 setlocal ENABLEEXTENSIONS & pushd .
 set D=%CD%\fstest
+set BIN=%CD%\bin\lookfs64.exe
 set F=%D%\hello.txt
 set D2=%D%\hello.dir
 if exist "%D%" rd /s /q "%D%"
@@ -8,5 +9,6 @@ md "%D%"
 md "%D2%"
 echo. > "%F%"
 for %%i in (foo bar baz) do @( echo %%i > "%D%:%%i" && echo %%i > "%D%\%%i" && echo %%i > "%D2%\%%i" && echo %%i > "%F%:%%i" )
-mklink /D "%D%\hello.junction" "%D2%"
+mklink /J "%D%\hello.junction" "%D2%"
+"%BIN%" --nologo --verbose "%D%\hello.junction"
 popd & endlocal & goto :EOF
