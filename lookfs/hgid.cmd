8bea6a035b73815beeef3e2286b40f7fd83057a7
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-13 23:34:39 +0300

    Update hgid.cmd

--- lookfs/hgid.cmd
+++ lookfs/hgid.cmd
@@ -2,9 +2,9 @@
 setlocal ENABLEEXTENSIONS & pushd .
 set HGTIPFILE="%~dp0hgid.h"
 echo.> %HGTIPFILE%
-for /f %%i in ('hg id -i') do @echo #define HG_REV_ID "%%i">> %HGTIPFILE%
-for /f %%i in ('hg id -n') do @echo #define HG_REV_NO %%i>>   %HGTIPFILE%
-for /f %%i in ('hg id -n') do @echo #define HG_REV_NO_NUMERIC %%i -0>>   %HGTIPFILE%
-for /f %%i in ('hg log -l 1 -r . -T {node}') do @echo #define HG_FULLID "%%i">> %HGTIPFILE%
+@echo #define HG_REV_ID 1>> %HGTIPFILE%
+@echo #define HG_REV_NO 1>>   %HGTIPFILE%
+@echo #define HG_REV_NO_NUMERIC 1 -0>>   %HGTIPFILE%
+@echo #define HG_FULLID 1>> %HGTIPFILE%
 if exist %HGTIPFILE% type %HGTIPFILE%
 popd & endlocal & goto :EOF
