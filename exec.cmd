91f550fe9f34eb2f62895b7120a0fecd16f187dd
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-29 00:48:20 +0300

    Update exec.cmd

--- exec.cmd
+++ exec.cmd
@@ -3,7 +3,6 @@ set NASM_PREFIX="C:\Users\runneradmin\nasm\"
 set CLANG_BIN="C:\Program Files\LLVM\bin\"
 set VSINSTALLDIR="C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
 cd edk2
-edksetup.bat Rebuild
 call edksetup.bat
 build -a X64 -t VS2019 -b NOOPT
 echo "-------------------------------------------------"
