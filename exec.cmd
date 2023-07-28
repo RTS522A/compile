3bfe933c23bfab9370f128b89c7b03d239cb18bc
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-29 00:07:33 +0300

    Update exec.cmd

--- exec.cmd
+++ exec.cmd
@@ -1,7 +1,7 @@
 iasl.exe -v
 set NASM_PREFIX=C:\Users\runneradmin\nasm\
 cd edk2
-edksetup.bat
+call edksetup.bat
 echo "-------------------------------------------------"
 build -w -a X64 -t VS2022 -b NOOPT -p MiniVisorPkg\Builds\Platform\EFI\MiniVisorPkg.dsc
 dir /s MiniVisorDxe.efi
