c08681ac9319a66356fd7370884867b200328741
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-28 23:59:14 +0300

    Create exec.cmd

---
+++ exec.cmd
@@ -0,0 +1,7 @@
+iasl.exe -v
+set NASM_PREFIX=C:\Users\runneradmin\nasm\
+cd edk2
+edksetup.bat
+echo "-------------------------------------------------"
+build -w -a X64 -t VS2022 -b NOOPT -p MiniVisorPkg\Builds\Platform\EFI\MiniVisorPkg.dsc
+dir /s MiniVisorDxe.efi
