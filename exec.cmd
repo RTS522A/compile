240a54d78b55a4b251f3ee85ad02a51bbe783630
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-29 00:23:17 +0300

    Update exec.cmd

--- exec.cmd
+++ exec.cmd
@@ -4,6 +4,7 @@ set CLANG_BIN="C:\Program Files\LLVM\bin\"
 cd edk2
 edksetup.bat Rebuild
 call edksetup.bat
+build -a X64 -t VS2019 -b NOOPT
 echo "-------------------------------------------------"
 build -w -a X64 -t VS2019 -b NOOPT -p MiniVisorPkg\Builds\Platform\EFI\MiniVisorPkg.dsc
 dir /s MiniVisorDxe.efi
