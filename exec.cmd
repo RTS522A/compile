94490261385b12fab60b1929a2b85318ae6f8c86
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-29 00:20:03 +0300

    Update exec.cmd

--- exec.cmd
+++ exec.cmd
@@ -2,6 +2,7 @@ iasl.exe -v
 set NASM_PREFIX="C:\Users\runneradmin\nasm\"
 set CLANG_BIN="C:\Program Files\LLVM\bin\"
 cd edk2
+edksetup.bat Rebuild
 call edksetup.bat
 echo "-------------------------------------------------"
 build -w -a X64 -t VS2019 -b NOOPT -p MiniVisorPkg\Builds\Platform\EFI\MiniVisorPkg.dsc
