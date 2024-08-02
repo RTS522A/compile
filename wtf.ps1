c0681ff591debae954ee2df4b93f9a94ed749c0c
RTS522A <95965159+RTS522A@users.noreply.github.com>
2024-08-02 23:58:15 +0300

    Create wtf.ps1

---
+++ wtf.ps1
@@ -0,0 +1,16 @@
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    Write-Output "Extracting $zipfile"
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+function Download
+{
+    param([string]$uri, [string]$outfile)
+    Write-Output "Downloading $uri"
+    $wc = New-Object net.webclient
+    $wc.Downloadfile($uri, $outfile)
+}
+Download "https://www.nuget.org/api/v2/package/wtl/10.0.10320" ".\wtl.zip"
+Unzip ".\wtl.zip" ".\packages\wtl.10.0.10320\"
