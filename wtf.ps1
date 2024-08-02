8f00c1880e6f67d3113265bd7dd319ccca18f818
RTS522A <95965159+RTS522A@users.noreply.github.com>
2024-08-03 00:30:25 +0300

    Update wtf.ps1

--- wtf.ps1
+++ wtf.ps1
@@ -15,7 +15,13 @@ function Download
 Download "https://www.nuget.org/api/v2/package/wtl/10.0.10320" ".\wtl.zip"
 Download "https://www.nuget.org/api/v2/package/Microsoft.Windows.ImplementationLibrary/1.0.201120.3" ".\mwli.zip"
 Unzip ".\wtl.zip" "D:\a\compile\compile\WMIExplorer\WTLHelper\packages\wtl.10.0.10320"
+Remove-Item -Path "D:\a\compile\compile\WMIExplorer\WTLHelper\packages\wtl.10.0.10320\Samples" -Recurse
 Unzip ".\wtl.zip" "D:\a\compile\compile\WMIExplorer\WTLHelper\..\packages\wtl.10.0.10320"
+Remove-Item -Path "D:\a\compile\compile\WMIExplorer\WTLHelper\..\packages\wtl.10.0.10320\Samples" -Recurse
 Unzip ".\wtl.zip" "D:\a\compile\compile\WMIExplorer\WTLHelper\..\..\packages\wtl.10.0.10320"
+Remove-Item -Path "D:\a\compile\compile\WMIExplorer\WTLHelper\..\..\packages\wtl.10.0.10320\Samples" -Recurse
+Unzip ".\wtl.zip" "D:\a\compile\compile\WMIExplorer\WTLHelper\..\..\..\packages\wtl.10.0.10320"
+Remove-Item -Path "D:\a\compile\compile\WMIExplorer\WTLHelper\..\..\..\packages\wtl.10.0.10320\Samples" -Recurse
 Unzip ".\mwli.zip" "D:\a\compile\compile\WMIExplorer\WTLHelper\..\..\packages\Microsoft.Windows.ImplementationLibrary.1.0.201120.3"
+Unzip ".\mwli.zip" "D:\a\compile\compile\WMIExplorer\WTLHelper\..\..\..\packages\Microsoft.Windows.ImplementationLibrary.1.0.201120.3"
 dir "D:\a\compile\compile\WMIExplorer\"
