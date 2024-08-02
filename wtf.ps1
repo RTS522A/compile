ebe850dca88193f23304136ca39e6ee3c24dec20
RTS522A <95965159+RTS522A@users.noreply.github.com>
2024-08-03 01:05:03 +0300

    Update wtf.ps1

--- wtf.ps1
+++ wtf.ps1
@@ -15,8 +15,10 @@ function Download
 
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-Install-Module -Name NuGet -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-Package Microsoft.Windows.ImplementationLibrary -Version 1.0.201120.3
 Install-PackageProvider -Name NuGet -Force
+Install-Module -Name NuGet -Force
 Install-Package Microsoft.Windows.ImplementationLibrary -Version 1.0.201120.3
 Install-Package wtl -Version 10.0.1032
 Download "https://www.nuget.org/api/v2/package/wtl/10.0.10320" ".\wtl.zip"
